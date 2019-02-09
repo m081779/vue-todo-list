545d94c3f52b48aa685d9da829d931ef9b475d85
m081779 <marcoprincipio@hotmail.com>
2019-02-08 22:16:20 -0500

    Fixed formatting across several files

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="about">
-    <h1>About</h1>
-    <p>This The TodoList app v1.0.0.  It is part of the Traversy Media Vue Crash Course series on YouTube</p>
-  </div>
+	<div class="about">
+		<h1>About</h1>
+		<p>This The TodoList app v1.0.0.  It is part of the Traversy Media Vue Crash Course series on YouTube</p>
+	</div>
 </template>
