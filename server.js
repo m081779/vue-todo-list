06aba9dfe35ef45e4f32c6c894c8a8eec642364b
m081779 <marcoprincipio@hotmail.com>
2019-02-08 23:13:37 -0500

    Cleaned up some code in server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
 var serveStatic = require('serve-static');
+
+const PORT = process.env.PORT || 3000;
 
 app.use(serveStatic(__dirname + "/dist"));
 app.listen(PORT, () => {
