1b169360f4d458b906d54ed7012116360e1adfb1
m081779 <marcoprincipio@hotmail.com>
2019-02-08 22:15:56 -0500

    Fixed formatting across several files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -38,35 +38,35 @@ export default {
   },
   created () {
     const todos = axios
-                    .get('https://jsonplaceholder.typicode.com/todos?_limit=5')
-                    .then(res => this.todos = res.data)
-                    .catch(err => console.log(err));
+                  .get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+                  .then(res => this.todos = res.data)
+                  .catch(err => console.log(err));
   }
 };
 </script>
 
 <style>
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
+* {
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;
+}
 
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.4;
-  }
+body {
+	font-family: Arial, Helvetica, sans-serif;
+	line-height: 1.4;
+}
 
-  .btn {
-    display: inline-block;
-    border: none;
-    background: #555;
-    color: #fff;
-    padding: 7px 20px;
-    cursor: pointer;
-  }
+.btn {
+	display: inline-block;
+	border: none;
+	background: #555;
+	color: #fff;
+	padding: 7px 20px;
+	cursor: pointer;
+}
 
-  .btn:hover {
-    background: #666;
-  }
+.btn:hover {
+	background: #666;
+}
 </style>
